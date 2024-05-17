2a24e5f99e3dbf0223738c4b91a5f5fb7bb38869
Alex1445085 <n1445085@yandex.ru>
2024-05-17 10:48:40 +0300

    changes in data in "paper"

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         var dog = 8.0;
         var cat = 3.6;
-        var paper = 763789;
+        var paper = 7637891;
         System.out.println("dog = " + dog);
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
@@ -16,7 +16,7 @@ public class Main {
 
         dog = dog - 3.5;
         cat = cat - 1.6;
-        paper = paper - 7639;
+        paper = paper - 12639;
         System.out.println("dog = " + dog);
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
